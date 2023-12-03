256c7416447e106d2808f7f713852557940deecf
Solowave <50268030+Solowave@users.noreply.github.com>
2023-12-03 13:45:17 -0500

    working better

--- simulate/simulate.py
+++ simulate/simulate.py
@@ -1,7 +1,7 @@
 from enum import IntEnum
 import numpy as np
 
-from orderbook.order_book import OrderBook
+from simulate.orderbook.order_book import OrderBook
 
 class OrderEvents:
     Limit=1
@@ -12,18 +12,16 @@ class Simulation:
     """
     Simulations Birth Death Process of orderbook
     """
-    def __init__(self, process_rates, starting_condititons = None):
+    def __init__(self, process_rates, average_event_size, depth=3):
         self.process_rates = process_rates
 
         self.orderbook = OrderBook()
-
-        # TODO: Add starting conditiions to orderbook if specified
-        if(starting_condititons != None):
-            print("Starting Conditions not yet implemented")
     
         self.time = 0.
         self.orderbook_history = [self.orderbook.get_mkt_depth(3)]
         self.time_history = [0.]
+        self.average_event_size = average_event_size
+        self.depth = depth
 
     def next_event(self):
         """
@@ -33,12 +31,30 @@ class Simulation:
         event: int, 0 means birth and 1 means death.
         """
 
+        top_of_orderbook = self.orderbook.get_mkt_depth(1)[0]
+        
+        # depth = 0
+
+        # for depth in range(0, self.depth):
+
+        if(top_of_orderbook and top_of_orderbook[0] and not top_of_orderbook[0].is_empty()):
+            ask_size = top_of_orderbook[0][1]
+        else:
+            ask_size = 0
+        
+        if(top_of_orderbook and top_of_orderbook[1] and not top_of_orderbook[1].is_empty()):
+            bid_size = top_of_orderbook[1][1]
+        else:
+            bid_size = 0
+
+        current_bid_queue =  bid_size / self.average_event_size
+        current_ask_queue =  ask_size / self.average_event_size
 
         # Gets the b/d rates at depth = 0
-        k_bid_limit = self.process_rates[0][0]
-        k_bid_market = self.process_rates[0][1]
-        k_bid_cancel = self.process_rates[0][2]
-        
+        k_bid_limit = self.process_rates[0][0][current_bid_queue]
+        k_bid_market = self.process_rates[0][1][current_bid_queue]
+        k_bid_cancel = self.process_rates[0][2][current_bid_queue]
+
         t_limit = np.random.exponential(1/k_bid_limit)    # draw a random number from exponential dist as putative birth time
         
         t_market = np.random.exponential(1/k_bid_market)    # draw a random number from exponential dist as putative death time
@@ -57,10 +73,10 @@ class Simulation:
 
             if event == OrderEvents.Limit:    # birth happens
                 self.orderbook.submit_order('lmt', 'bid', 1, midprice - tick_size, 0)
-                self.orderbook.submit_order('lmt', 'ask', 1, midprice - tick_size, 0)
+                self.orderbook.submit_order('lmt', 'ask', 1, midprice + tick_size, 0)
             elif event == OrderEvents.Market:    # death happens
                 self.orderbook.submit_order('mkt', 'bid', 0.5, midprice - tick_size, 0)
-                self.orderbook.submit_order('mkt', 'ask', 0.5, midprice - tick_size, 0)
+                self.orderbook.submit_order('mkt', 'ask', 0.5, midprice + tick_size, 0)
             
-            self.time_hist.append(self.time)    # record time of event
-            self.pop_hist.append(self.orderbook.get_mkt_depth(3))    # record population size after event
+            self.time_history.append(self.time)    # record time of event
+            self.orderbook_history.append(self.orderbook.get_mkt_depth(3))    # record population size after event
