1210da3bd59e1d8609cbb046f52cdbd901089c71
Solowave <50268030+Solowave@users.noreply.github.com>
2024-04-07 11:27:35 -0400

    main

--- simulate/simulate.py
+++ simulate/simulate.py
@@ -1,5 +1,6 @@
 from enum import IntEnum
 import numpy as np
+import math
 
 from simulate.orderbook.order_book import OrderBook
 
@@ -12,7 +13,7 @@ class Simulation:
     """
     Simulations Birth Death Process of orderbook
     """
-    def __init__(self, process_rates, average_event_size, depth=3):
+    def __init__(self, process_rates, depth=3, tick_size = 0.01, midprice = 100, length=100):
         self.process_rates = process_rates
 
         self.orderbook = OrderBook()
@@ -20,8 +21,10 @@ class Simulation:
         self.time = 0.
         self.orderbook_history = [self.orderbook.get_mkt_depth(3)]
         self.time_history = [0.]
-        self.average_event_size = average_event_size
         self.depth = depth
+        self.tick_size = tick_size
+        self.midprice = midprice
+        self.length = length
 
     def next_event(self):
         """
@@ -31,52 +34,56 @@ class Simulation:
         event: int, 0 means birth and 1 means death.
         """
 
-        top_of_orderbook = self.orderbook.get_mkt_depth(1)[0]
-        
-        # depth = 0
+        depth = 0
 
-        # for depth in range(0, self.depth):
+        best_ask = self.orderbook.get_mkt_depth(1)[0]
 
-        if(top_of_orderbook and top_of_orderbook[0] and not top_of_orderbook[0].is_empty()):
-            ask_size = top_of_orderbook[0][1]
-        else:
+        lambda_length = 100
+
+        if len(best_ask) == 0:
             ask_size = 0
-        
-        if(top_of_orderbook and top_of_orderbook[1] and not top_of_orderbook[1].is_empty()):
-            bid_size = top_of_orderbook[1][1]
         else:
-            bid_size = 0
+            ask_size = min(math.ceil(best_ask[0][1]), lambda_length - 1)
+        
+        best_bid = self.orderbook.get_mkt_depth(1)[1]
 
-        current_bid_queue =  bid_size / self.average_event_size
-        current_ask_queue =  ask_size / self.average_event_size
+        if len(best_bid) == 0:
+            bid_size = 0
+        else:
+            bid_size = min(math.ceil(best_ask[0][1]), lambda_length - 1)
 
         # Gets the b/d rates at depth = 0
-        k_bid_limit = self.process_rates[0][0][current_bid_queue]
-        k_bid_market = self.process_rates[0][1][current_bid_queue]
-        k_bid_cancel = self.process_rates[0][2][current_bid_queue]
+        k_bid_limit = self.process_rates[depth][0][0][bid_size]
+        k_bid_market = self.process_rates[depth][0][1][bid_size]
+        k_bid_cancel = self.process_rates[depth][0][2][bid_size]
 
-        t_limit = np.random.exponential(1/k_bid_limit)    # draw a random number from exponential dist as putative birth time
-        
-        t_market = np.random.exponential(1/k_bid_market)    # draw a random number from exponential dist as putative death time
+        # lambdas[depth_level][OrderbookIndexes.Ask][OrderbookTypes.Limit][queue_length]
+        k_ask_limit = self.process_rates[depth][0][0][ask_size]
+        k_ask_market = self.process_rates[depth][0][1][ask_size]
+        k_ask_cancel = self.process_rates[depth][0][2][ask_size]
+
+        tb_limit = np.random.exponential(k_bid_limit)    # draw a random number from exponential dist as putative birth time        
+        tb_market = np.random.exponential(k_bid_market)    # draw a random number from exponential dist as putative death time
+        tb_cancel = np.random.exponential(k_bid_cancel)    # draw a random number from exponential dist as putative death time
+        # Test if 1/k works for the time interval
         # t_cancel = np.random.exponential(1/k_bid_cancel)    # draw a random number from exponential dist as putative death time
 
-        if t_limit < t_market:    # birth happens first
-            return t_limit, OrderEvents.Limit
-        else:    # death happens first
-            return t_market, OrderEvents.Market
+        ta_limit = np.random.exponential(k_ask_limit)    # draw a random number from exponential dist as putative birth time        
+        ta_market = np.random.exponential(k_ask_market)    # draw a random number from exponential dist as putative death time
+        ta_cancel = np.random.exponential(k_ask_cancel)    # draw a random number from exponential dist as putative death time
+        # Test if 1/k works for the time interval
+        # t_cancel = np.random.exponential(1/k_bid_cancel)    # draw a random number from exponential dist as putative death time
+
+        self.orderbook.submit_order('lmt', 'bid', tb_limit, self.midprice - self.tick_size, 0)
+        self.orderbook.submit_order('mkt', 'bid', tb_market, self.midprice - self.tick_size, 0)
+        self.orderbook.submit_order('mkt', 'bid', tb_cancel, self.midprice - self.tick_size, 0)
         
-    def run(self, midprice=100, length=100, tick_size = 0.01):
+        self.orderbook.submit_order('lmt', 'ask', ta_limit, self.midprice + self.tick_size, 0)
+        self.orderbook.submit_order('mkt', 'ask', ta_market, self.midprice + self.tick_size, 0)
+        self.orderbook.submit_order('mkt', 'ask', ta_cancel, self.midprice + self.tick_size, 0)
 
-        while self.time < length:
-            tau, event = self.next_event()    # draw next event
-            self.time += tau    # update time
-
-            if event == OrderEvents.Limit:    # birth happens
-                self.orderbook.submit_order('lmt', 'bid', 1, midprice - tick_size, 0)
-                self.orderbook.submit_order('lmt', 'ask', 1, midprice + tick_size, 0)
-            elif event == OrderEvents.Market:    # death happens
-                self.orderbook.submit_order('mkt', 'bid', 0.5, midprice - tick_size, 0)
-                self.orderbook.submit_order('mkt', 'ask', 0.5, midprice + tick_size, 0)
-            
+    def run(self):
+        for i in range(0, self.length):
+            self.next_event()
             self.time_history.append(self.time)    # record time of event
             self.orderbook_history.append(self.orderbook.get_mkt_depth(3))    # record population size after event
